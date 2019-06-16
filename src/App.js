500d34d4db442fc1bc4664851cd1a4aad925a478
Hackernine <akhyl.78@gmail.com>
2019-06-17 00:05:25 +0545

    initial react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render() {
+    return(
+      <h1>My React App</h1>
+    );
+    };
+}
+
+export default App;
