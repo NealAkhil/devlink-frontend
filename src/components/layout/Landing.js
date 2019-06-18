195086d13ca060c87781af9c7d39f12b420bfaa5
Hackernine <akhyl.78@gmail.com>
2019-06-18 14:06:22 +0545

    Style for Navbar and landing page added

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -3,14 +3,17 @@ import '../../css/Landing.css'
 class Landing extends Component {
     render() {
         return (
-            <div className="main-land">
+            <div className="main-div">
                 <div className="container">
                     <div className="row container-land">
-                            <div className="col-sm">
+                            <div className="col-sm left-div">
                                 <h1 className="heading-text"> Let's get connected professionally</h1>
+                                <button className="btn">
+                                    Get Started
+                                </button>
                             </div>
-                            <div className="col-sm ml-auto bggg">
-                                One of three columns
+                            <div className="col-sm ml-auto right-div">
+                                
                             </div>
                     </div>
                 </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
 import '../../css/Navbar.css';
+import {Link} from 'react-router-dom';
 class Navbar extends Component {
     render() {
 
         return (
-            <div className="nav-el fixed-top">
-                <nav className="navbar navbar-expand-md">
+                <nav className="navbar navbar-expand-md nav-el navbar-offse" data-spy="affix">
                     <div className="container">
                         <button className="navbar-toggler res-menu ml-auto" data-toggle="collapse" data-target="#collapse_target">
-                            <span className="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                            <span className="navbar-toggler-icon"><i className="fa fa-bars" aria-hidden="true"></i></span>
                         </button>
                         <div className="collapse navbar-collapse" id="collapse_target">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#"> About</a>
+                                    <Link className="nav-link" to="/about"> About</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#"> Explore</a>
+                                    <Link className="nav-link" to="/explore"> Explore</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#"> Developers</a>
+                                    <Link className="nav-link" to="/developers"> Developers</Link>
                                 </li>
                             </ul>
                             
@@ -28,16 +28,15 @@ class Navbar extends Component {
                                     <input type="text" placeholder="Search Devlink" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="register.html">Sign Up</a>
+                                    <Link className="nav-link" to="/register">Sign Up</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="login.html">Login</a>
+                                    <Link className="nav-link" to="/login">Login</Link>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
-            </div>
         )
     }
 }
